6e481e057251f441f74cf22c8d8dbcefc4bea022
“Anton <a.kaymakchi@dodopizza.com>
2020-12-31 13:08:11 +0300

    Extract common flags

---
+++ cli/cmd/common_options.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"strings"
+
+	"github.com/dodopizza/kubectl-shovel/internal/version"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+type commonOptions struct {
+	image   string
+	podName string
+	output  string
+
+	kubeFlags *genericclioptions.ConfigFlags
+}
+
+func (options *commonOptions) newCommonFlags(tool string) *pflag.FlagSet {
+	flags := pflag.NewFlagSet("common", pflag.ExitOnError)
+	flags.StringVar(
+		&options.podName,
+		"pod-name",
+		options.podName,
+		"Target pod",
+	)
+	panicOnError(cobra.MarkFlagRequired(flags, "pod-name"))
+
+	flags.StringVarP(
+		&options.output,
+		"output",
+		"o",
+		"./"+
+			currentTime()+
+			"."+
+			tool,
+		"Output file",
+	)
+
+	flags.StringVar(
+		&options.image,
+		"image",
+		strings.Join(
+			[]string{
+				dumperImageName,
+				version.GetVersion(),
+			},
+			":",
+		),
+		"Image of dumper to use for job",
+	)
+	options.kubeFlags = genericclioptions.NewConfigFlags(false)
+	options.kubeFlags.AddFlags(flags)
+
+	return flags
+}
--- cli/main.go
+++ cli/main.go
@@ -4,13 +4,9 @@ import (
 	"os"
 
 	"github.com/dodopizza/kubectl-shovel/cli/cmd"
-	"github.com/spf13/pflag"
 )
 
 func main() {
-	flags := pflag.NewFlagSet("kubectl-shovel", pflag.ExitOnError)
-	pflag.CommandLine = flags
-
 	rootCmd := cmd.NewShovelCommand()
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
