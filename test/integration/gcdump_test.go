29ae3ebf8fc4d6f81a9893097221ae521b258892
“Anton <a.kaymakchi@dodobrands.io>
2021-03-30 14:15:11 +0300

    New job name with uuid

--- test/integration/gcdump_test.go
+++ test/integration/gcdump_test.go
@@ -37,6 +37,7 @@ func Test_GCDumpSubcommand(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 			teardown := setup(t, tc.pod)
 			defer teardown()
 			dir, _ := ioutil.TempDir("", tempDirPattern)
