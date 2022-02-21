29493b6c7d088498bc72053130dd6951998953d6
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-02-21 16:29:56 +0300

    chore(tests): add additional test cases for shared mounts
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- test/integration/helpers_test.go
+++ test/integration/helpers_test.go
@@ -1,3 +1,4 @@
+//go:build integration
 // +build integration
 
 package integration_test
@@ -8,9 +9,10 @@ import (
 	"path/filepath"
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	core "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8s "k8s.io/client-go/kubernetes"
+
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 
@@ -23,7 +25,7 @@ import (
 var (
 	sampleAppName       = "sample-app"
 	sampleAppImage      = "mcr.microsoft.com/dotnet/core/samples:aspnetapp"
-	deletePolicy        = metav1.DeletePropagationForeground
+	deletePolicy        = meta.DeletePropagationForeground
 	targetContainerName = "target"
 	namespace           = "default"
 	dumperImage         = "kubectl-shovel/dumper-integration-tests"
@@ -48,53 +50,47 @@ func newTestKubeClient() *kubernetes.Client {
 	}
 }
 
-func setup(t *testing.T, pod *v1.Pod) func() {
+func setup(t *testing.T, pod *core.Pod) func() {
 	t.Helper()
-	k8s := newTestKubeClient()
+	k := newTestKubeClient()
 
 	fmt.Println("Deploying target pod to cluster...")
-	_, err := k8s.CoreV1().Pods(namespace).Create(
+	_, err := k.CoreV1().Pods(namespace).Create(
 		context.Background(),
 		pod,
-		metav1.CreateOptions{},
+		meta.CreateOptions{},
 	)
 	require.NoError(t, err)
 
 	fmt.Println("Waiting app to start...")
-	_, err = k8s.WaitPod(map[string]string{
-		"app": pod.Name,
-	})
+	_, err = k.WaitPod(pod.ObjectMeta.Labels)
 	require.NoError(t, err)
 
 	return func() {
-		_ = k8s.CoreV1().Pods(namespace).Delete(
+		_ = k.CoreV1().Pods(namespace).Delete(
 			context.TODO(),
 			pod.Name,
-			metav1.DeleteOptions{PropagationPolicy: &deletePolicy},
+			meta.DeleteOptions{PropagationPolicy: &deletePolicy},
 		)
 	}
 }
 
-func newRandomString() string {
-	return uuid.NewString()
+func generateRandomPodMeta() meta.ObjectMeta {
+	name := fmt.Sprintf("%s-%s", sampleAppName, uuid.NewString())
+
+	return meta.ObjectMeta{
+		Name: name,
+		Labels: map[string]string{
+			"app": name,
+		},
+	}
 }
 
-func sampleAppPod() *v1.Pod {
-	targetPodName := fmt.Sprintf(
-		"%s-%s",
-		sampleAppName,
-		newRandomString(),
-	)
-	labels := map[string]string{
-		"app": targetPodName,
-	}
-	return &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   targetPodName,
-			Labels: labels,
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
+func singleContainerPod() *core.Pod {
+	return &core.Pod{
+		ObjectMeta: generateRandomPodMeta(),
+		Spec: core.PodSpec{
+			Containers: []core.Container{
 				{
 					Name:  targetContainerName,
 					Image: sampleAppImage,
@@ -104,22 +100,11 @@ func sampleAppPod() *v1.Pod {
 	}
 }
 
-func multiContainerPod() *v1.Pod {
-	targetPodName := fmt.Sprintf(
-		"%s-%s",
-		sampleAppName,
-		newRandomString(),
-	)
-	labels := map[string]string{
-		"app": targetPodName,
-	}
-	return &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   targetPodName,
-			Labels: labels,
-		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{
+func multiContainerPod() *core.Pod {
+	return &core.Pod{
+		ObjectMeta: generateRandomPodMeta(),
+		Spec: core.PodSpec{
+			Containers: []core.Container{
 				{
 					Name:  targetContainerName,
 					Image: sampleAppImage,
@@ -127,6 +112,38 @@ func multiContainerPod() *v1.Pod {
 				{
 					Name:  "sidecar",
 					Image: "gcr.io/google_containers/pause-amd64:3.1",
+				},
+			},
+		},
+	}
+}
+
+func multiContainerPodWithSharedMount() *core.Pod {
+	return &core.Pod{
+		ObjectMeta: generateRandomPodMeta(),
+		Spec: core.PodSpec{
+			Containers: []core.Container{
+				{
+					Name:  targetContainerName,
+					Image: sampleAppImage,
+					VolumeMounts: []core.VolumeMount{
+						{Name: "shared-path-to-tmp", MountPath: "/tmp"},
+					},
+				},
+				{
+					Name:  "sidecar",
+					Image: "gcr.io/google_containers/pause-amd64:3.1",
+					VolumeMounts: []core.VolumeMount{
+						{Name: "shared-path-to-tmp", MountPath: "/tmp"},
+					},
+				},
+			},
+			Volumes: []core.Volume{
+				{
+					Name: "shared-path-to-tmp",
+					VolumeSource: core.VolumeSource{
+						EmptyDir: &core.EmptyDirVolumeSource{},
+					},
 				},
 			},
 		},
