7e7c52ca31ce770561ea3d0ff5c87a5f2019928a
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-02-26 22:08:24 +0300

    fix(dumper): fix time formatting for artifact name
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- dumper/cmd/launch.go
+++ dumper/cmd/launch.go
@@ -76,7 +76,7 @@ func (cb *CommandBuilder) launch() error {
 			cb.CommonOptions.PodName,
 			cb.CommonOptions.ContainerName,
 			filepath.Base(output),
-			time.Now().UTC().Format("2006-04-02-15-04-05"),
+			time.Now().UTC().Format("2006-01-02-15-04-05"),
 		)
 
 		if err := utils.MoveFile(output, outputHost); err != nil {
