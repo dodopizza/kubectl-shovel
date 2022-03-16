29677a13abd0cf909aff71eb2e87a64670a37806
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-03-16 16:33:56 +0300

    chore(flags): remove code duplication for createdump
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- internal/flags/create_dump.go
+++ internal/flags/create_dump.go
@@ -10,36 +10,15 @@ import (
 // https://github.com/dotnet/runtime/blob/main/docs/design/coreclr/botr/xplat-minidump-generation.md
 
 type createdump struct {
-	ProcessID int
-	Output    string
+	*DotnetToolSharedOptions
 
 	flagSet *pflag.FlagSet
 }
 
 func NewCreateDump() DotnetTool {
 	return &createdump{
-		ProcessID: 1,
+		DotnetToolSharedOptions: NewDotnetToolSharedOptions(),
 	}
-}
-
-func (cd *createdump) GetFlags() *pflag.FlagSet {
-	flagSet := pflag.NewFlagSet(cd.BinaryName(), pflag.ExitOnError)
-	flagSet.IntVarP(
-		&cd.ProcessID,
-		"process-id",
-		"p",
-		cd.ProcessID,
-		"The process ID to collect the trace from",
-	)
-	flagSet.StringVarP(
-		&cd.Output,
-		"output",
-		"o",
-		cd.Output,
-		"Output file",
-	)
-	cd.flagSet = flagSet
-	return cd.flagSet
 }
 
 func (cd *createdump) FormatArgs(args *Args) {
@@ -52,14 +31,6 @@ func (cd *createdump) FormatArgs(args *Args) {
 
 func (*createdump) SetAction(_ string) {
 	// omit action usage here because of create dump is not a dotnet tool (it's a runtime tool)
-}
-
-func (cd *createdump) SetOutput(output string) {
-	cd.Output = output
-}
-
-func (cd *createdump) SetProcessID(processID int) {
-	cd.ProcessID = processID
 }
 
 func (*createdump) BinaryName() string {
