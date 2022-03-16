0fbdeadeb8550d3edcfbd4f3c1336190eaec1153
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-03-16 16:48:01 +0300

    chore(flags): split file
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

---
+++ internal/flags/dotnet_tool_interface.go
@@ -0,0 +1,26 @@
+package flags
+
+import (
+	"github.com/spf13/pflag"
+)
+
+const (
+	FormatArgsTypeBinary = FormatArgsType("binary")
+	FormatArgsTypeTool   = FormatArgsType("tool")
+)
+
+type DotnetToolFactory func() DotnetTool
+type FormatArgsType string
+
+type DotnetTool interface {
+	DotnetToolFlagsFormatter
+	BinaryName() string
+	ToolName() string
+}
+
+type DotnetToolFlagsFormatter interface {
+	FormatArgs(args *Args, t FormatArgsType)
+	GetFlags() *pflag.FlagSet
+	SetOutput(output string)
+	SetProcessID(id int)
+}
