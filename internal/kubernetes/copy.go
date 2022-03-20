81919489a0ba21d6d6451a3ff2d9c731afe308ae
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-03-20 22:07:04 +0300

    chore(kubernetes): add container state description to pod waiting in case of error
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- internal/kubernetes/copy.go
+++ internal/kubernetes/copy.go
@@ -10,32 +10,32 @@ import (
 )
 
 // CopyFromPod - copy file from pod to local file
-func (k8s *Client) CopyFromPod(podName, podFilePath, localFilePath string) error {
-	from := buildPodPath(k8s.Namespace, podName, podFilePath)
+func (k *Client) CopyFromPod(podName, podFilePath, localFilePath string) error {
+	from := buildPodPath(k.Namespace, podName, podFilePath)
 
-	return k8s.copy(from, localFilePath)
+	return k.copy(from, localFilePath)
 }
 
 // CopyToPod - copy local file to pod
-func (k8s *Client) CopyToPod(localFilePath, podName, podFilePath string) error {
-	to := buildPodPath(k8s.Namespace, podName, podFilePath)
+func (k *Client) CopyToPod(localFilePath, podName, podFilePath string) error {
+	to := buildPodPath(k.Namespace, podName, podFilePath)
 
-	return k8s.copy(localFilePath, to)
+	return k.copy(localFilePath, to)
 }
 
-func buildPodPath(namespace, podName, podFilePath string) string {
-	return fmt.Sprintf("%s/%s:%s", namespace, podName, podFilePath)
-}
-
-func (k8s *Client) copy(from, to string) error {
+func (k *Client) copy(from, to string) error {
 	ioStreams := genericclioptions.IOStreams{
 		In:     &bytes.Buffer{},
 		Out:    &bytes.Buffer{},
 		ErrOut: os.Stdout,
 	}
 	opts := cp.NewCopyOptions(ioStreams)
-	opts.Clientset = k8s.Clientset
-	opts.ClientConfig = k8s.Config
+	opts.Clientset = k.Clientset
+	opts.ClientConfig = k.Config
 
 	return opts.Run([]string{from, to})
+}
+
+func buildPodPath(namespace, podName, podFilePath string) string {
+	return fmt.Sprintf("%s/%s:%s", namespace, podName, podFilePath)
 }
